6099f92a7e7da59937f1ebf6c4f33ac470a23b63
codeconcept <contact@codeconcept.fr>
2019-11-13 13:01:12 +0100

    Exact routing to home page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Link, Route } from 'react-router-dom'
 import './App.css';
 import Registration from './Registration';
 import Schedule from './Schedule';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
+      <Link to="/">Accueil</Link>{" "}
       <Link to="/registration">Inscriptions</Link>{" "}
       <Link to="/schedule">Programme</Link>
+      <Route exact path="/" component={Home} />
       <Route path="/registration" component={Registration} />
       <Route path="/schedule" component={Schedule} />
     </div>
