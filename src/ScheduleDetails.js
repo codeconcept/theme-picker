6342c47f7206fc7858aff9cedd422b81987e77ba
codeconcept <contact@codeconcept.fr>
2019-11-13 18:48:21 +0100

    Consuming several properties of the object passed as context

--- src/ScheduleDetails.js
+++ src/ScheduleDetails.js
@@ -4,15 +4,24 @@ import MessageContext from "./MessageContext";
 const ScheduleDetails = ({ activity }) => {
   const { category, title, town } = activity;
   return (
-    <div className="activity">
-      <h2>{title}</h2>
-      <div>
-        {category} - {town}
-      </div>
-      <MessageContext.Consumer>
-        {value => <div style={{ fontSize: 11 }}>{value.message}</div>}
-      </MessageContext.Consumer>
-    </div>
+    <MessageContext.Consumer>
+      {value => (
+        <div className="activity">
+          <h2>{title}</h2>
+          <div>
+            {category} - {town}
+          </div>
+          <div
+            style={{
+              fontSize: 12,
+              color: value.theme === "day" ? "#c12527" : "#e0cbc4"
+            }}
+          >
+            {value.message}
+          </div>
+        </div>
+      )}
+    </MessageContext.Consumer>
   );
 };
 
