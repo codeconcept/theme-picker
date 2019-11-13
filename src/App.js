a9053ae37d4f20434ce3c2290bca6aacd60f09a8
codeconcept <contact@codeconcept.fr>
2019-11-13 17:08:42 +0100

    select for night theme / day theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { Link, Route } from 'react-router-dom'
-import './App.css';
-import Registration from './Registration';
-import Schedule from './Schedule';
-import Home from './Home';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import "./App.css";
+import Registration from "./Registration";
+import Schedule from "./Schedule";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <Link to="/">Accueil</Link>{" "}
-      <Link to="/registration">Inscriptions</Link>{" "}
-      <Link to="/schedule/Dijon">Programmes de Dijon</Link>{" "}
-      <Link to="/schedule/Lyon">Programmes de Lyon</Link>{" "}
-      <Link to="/schedule">Programmes</Link>
-      <Route exact path="/" component={Home} />
-      <Route path="/registration" component={Registration} />
-      <Route path="/schedule/:town?" component={Schedule} />
-    </div>
-  );
+class App extends React.Component {
+  state = { theme: "day" };
+  render() {
+    const { theme } = this.state;
+    return (
+      <div className={theme === "day" ? "App day" : "App night"}>
+        <Link to="/">Accueil</Link> <Link to="/registration">Inscriptions</Link>{" "}
+        <Link to="/schedule/Dijon">Programmes de Dijon</Link>{" "}
+        <Link to="/schedule/Lyon">Programmes de Lyon</Link>{" "}
+        <Link to="/schedule">Programmes</Link>{" "}
+        <label htmlFor="theme">
+          thème{" "}
+          <select
+            id="name"
+            value={theme}
+            onChange={e => this.setState({ theme: e.target.value })}
+          >
+            <option value="day">jour</option>
+            <option value="night">nuit</option>
+          </select>
+        </label>
+        <Route exact path="/" component={Home} />
+        <Route path="/registration" component={Registration} />
+        <Route path="/schedule/:town?" component={Schedule} />
+      </div>
+    );
+  }
 }
 
 export default App;
