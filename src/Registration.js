d138a8945a795fa2b6afbf6ecf4c1886c1690a17
codeconcept <contact@codeconcept.fr>
2019-11-13 12:54:11 +0100

    Configuring routing with React Router v5

---
+++ src/Registration.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Registration extends React.Component {
+  state = {  }
+  render() { 
+    return (<h1>Registration page</h1>);
+  }
+}
+ 
+export default Registration;
