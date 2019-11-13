c24dccc61aaf816bd4f962ca82f61a28d820a93b
codeconcept <contact@codeconcept.fr>
2019-11-13 18:33:11 +0100

    Context to provide data and consume it directly from grand child

--- src/App.js
+++ src/App.js
@@ -4,45 +4,52 @@ import "./App.css";
 import Registration from "./Registration";
 import Schedule from "./Schedule";
 import Home from "./Home";
+import MessageContext from "./MessageContext";
 
 class App extends React.Component {
-  state = { theme: "day" };
+  state = {
+    theme: "day",
+    message: "Tickets disponibles auprès du CE"
+  };
+
   render() {
     const { theme } = this.state;
     return (
-      <div className={theme === "day" ? "App day" : "App night"}>
-        <NavLink exact activeClassName="selected" to="/">
-          Accueil
-        </NavLink>{" "}
-        <NavLink activeClassName="selected" to="/registration">
-          Inscriptions
-        </NavLink>{" "}
-        <NavLink exact activeClassName="selected" to="/schedule/Dijon">
-          Programmes de Dijon
-        </NavLink>{" "}
-        <NavLink exact activeClassName="selected" to="/schedule/Lyon">
-          Programmes de Lyon
-        </NavLink>{" "}
-        <NavLink exact activeClassName="selected" to="/schedule">
-          Programmes
-        </NavLink>{" "}
-        <label htmlFor="theme">
-          thème{" "}
-          <select
-            id="name"
-            value={theme}
-            onChange={e => this.setState({ theme: e.target.value })}
-          >
-            <option value="day">jour</option>
-            <option value="night">nuit</option>
-          </select>
-        </label>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/registration" component={Registration} />
-          <Route path="/schedule/:town?" component={Schedule} />
-        </Switch>
-      </div>
+      <MessageContext.Provider value={this.state}>
+        <div className={theme === "day" ? "App day" : "App night"}>
+          <NavLink exact activeClassName="selected" to="/">
+            Accueil
+          </NavLink>{" "}
+          <NavLink activeClassName="selected" to="/registration">
+            Inscriptions
+          </NavLink>{" "}
+          <NavLink exact activeClassName="selected" to="/schedule/Dijon">
+            Programmes de Dijon
+          </NavLink>{" "}
+          <NavLink exact activeClassName="selected" to="/schedule/Lyon">
+            Programmes de Lyon
+          </NavLink>{" "}
+          <NavLink exact activeClassName="selected" to="/schedule">
+            Programmes
+          </NavLink>{" "}
+          <label htmlFor="theme">
+            thème{" "}
+            <select
+              id="name"
+              value={theme}
+              onChange={e => this.setState({ theme: e.target.value })}
+            >
+              <option value="day">jour</option>
+              <option value="night">nuit</option>
+            </select>
+          </label>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/registration" component={Registration} />
+            <Route path="/schedule/:town?" component={Schedule} />
+          </Switch>
+        </div>
+      </MessageContext.Provider>
     );
   }
 }
--- src/ScheduleDetails.js
+++ src/ScheduleDetails.js
@@ -1,13 +1,17 @@
 import React from "react";
+import MessageContext from "./MessageContext";
 
 const ScheduleDetails = ({ activity }) => {
   const { category, title, town } = activity;
   return (
     <div className="activity">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <div>
         {category} - {town}
       </div>
+      <MessageContext.Consumer>
+        {value => <div style={{ fontSize: 11 }}>{value.message}</div>}
+      </MessageContext.Consumer>
     </div>
   );
 };
